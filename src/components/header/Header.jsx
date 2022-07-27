cee69d362f18f60ecbf1b6052af76639493e943a
sebastian <sebastian.silva.agurto@hotmail.com>
2022-07-27 16:35:50 -0500

    Header/Fixed navigation toggle

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
-import {ToggleTheme} from '../Toggle';
+import {ToggleTheme, ToggleDropDown} from '../Toggle';
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-full h-[45px] lg:h-[70px] mt-[20px]">
+    <div className="w-full h-[45px] lg:h-[70px] mt-[20px] relative">
       <nav className="w-full h-full nav-style">
-        <div className="w-fit my-auto">
-          <span className="font-bold lg:text-xl">Sebastian</span>
-          <span className="font-light lg:text-xl">Silva</span>
-        </div>
-        <div className="flex my-auto space-x-2 md:space-x-4">
+        {
+          isOpen ? (
+            <div className="w-fit my-auto hidden md:block">
+              <span className="font-bold lg:text-xl">Sebastian</span>
+              <span className="font-light lg:text-xl">Silva</span>
+            </div>
+          ) : (
+            <div className="w-fit my-auto">
+              <span className="font-bold lg:text-xl">Sebastian</span>
+              <span className="font-light lg:text-xl">Silva</span>
+            </div>
+          )
+        }
+        <div className="flex my-auto space-x-2 md:space-x-4 transition-all">
+          <ToggleDropDown state={isOpen} _callback={() => setIsOpen(!isOpen)}/>
           <ToggleTheme />
         </div>
       </nav>
