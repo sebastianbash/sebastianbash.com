b08f908a58546df7be5bbce528df5c52d9c35221
sebastian <sebastian.silva.agurto@hotmail.com>
2022-07-27 00:09:56 -0500

    Adding router and routes to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <h1>Hello from sebastianbash.com</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<h3>Hello from Home</h3>} />
+      </Route>
+    </Routes>
   );
 }
 
