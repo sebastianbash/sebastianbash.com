5c70ada00cde2c522ce09c1a6cca31cdda245b48
sebastian <sebastian.silva.agurto@hotmail.com>
2022-07-27 20:07:50 -0500

    Hero/Added hero structure with samples and setup inter font

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from '../hero/Hero';
+
+export default function Landing() {
+  return (
+    <>
+        {/* Hero section */}
+        <Hero />
+        {/* Project section */}
+        {/* About section */}
+        {/* Contact section */}
+    </>
+  )
+}
