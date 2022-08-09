8e97fd7f060d3f804f61024dc89f50c1f967cd67
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-09 11:35:53 -0500

    Pass

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext';
 import App from './App'
 import './index.css'
+import 'flowbite'
+import 'animate.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.jsx",
+    './index.html',
+    './src/**/*.jsx',
+    'node_modules/flowbite-react/**/*.jsx'
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  theme: {},
+  plugins: [
+    // eslint-disable-next-line global-require
+    require('flowbite/plugin'),
+  ],
 }
