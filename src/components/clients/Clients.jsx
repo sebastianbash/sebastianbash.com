f2d5110678cda43a813450aad85ab66bd325c57a
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-09 12:25:47 -0500

    Creadted component Client

---
+++ src/components/clients/Clients.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Clients() {
+  return (
+    <div className="container">
+        <div className="cli-title">
+            <h3 className="text-[2rem] leading-[2rem] tracking-[-.96px] text-center font-[600]">Clientes</h3>
+        </div>
+        <div className="cli-brands">
+            <div className="logo-card">
+                Google
+            </div>
+        </div>
+    </div>
+  )
+}
