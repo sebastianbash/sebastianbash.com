b9509b6bb4f7a863c8e08d5ee9217016f24302c4
sebastian <sebastian.silva.agurto@hotmail.com>
2022-08-29 21:00:40 -0500

    Project routes added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Landing from './components/landing/Landing';
+import Project from './pages/projects/Projects';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Landing />} />
+        <Route path="/projects/:projectId" element={<Project />} />
+        <Route path="*" element={<h2>Pagina no encontrada!</h2>} />
       </Route>
     </Routes>
   );
